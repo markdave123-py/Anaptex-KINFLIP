02d646deeee5b018e14b12ac1540463fc7a1e85e
markdave123-py <nwaewkudavid@gmail.com>
2023-04-06 17:27:48 +0100

    some changes

--- src/controllers/register.js
+++ src/controllers/register.js
@@ -1,12 +1,38 @@
-import { User } from "../models/user";
+import {Speaker, Attendee} from "../models/user";
 
 
 export const registerSpeaker = async (req, res) => {
     const { name , email, phoneNumber, country, brandName, socialMediaLink } = req.body
 
-    try {
-        
+    const userExsists = await Speaker.findOne({email: email})
+    .then(user => {
+        if (user){
+            console.log('user with this email already exists!!')
+        }else{
+            try {
+                const user = Speaker.create({
+                    name: name,
+                    email: email,
+                    phoneNumber: phoneNumber,
+                    country: country,
+                    brandName: brandName, 
+                    socialMediaLink: socialMediaLink
+                })
+                user.save()
+            } catch (error) {
+                console.log(error.message)
+            }
+        }
 
+    })
+
+   
+}
+
+export const registerAttendee = async (req, res) => {
+    const { name , email, phoneNumber, country, brandName, socialMediaLink } = req.body
+
+    try {
         const user = await User.create({
             name: name,
             email: email,
